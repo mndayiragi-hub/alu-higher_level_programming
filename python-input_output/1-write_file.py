f5dfde0084ae11de7e0eeb1a1b1d51b8dca27e28
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-07-29 05:03:48 -0700

    Add 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a write_file function"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file and return number of chars written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
