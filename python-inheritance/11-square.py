bf092e49eee85d94c9bf6c43d4d84bba5ec3a2de
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-07-29 04:57:19 -0700

    Fix PEP8 indentation in 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,5 +13,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return the string representation of the square"""
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                        self._Rectangle__height)
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height)
