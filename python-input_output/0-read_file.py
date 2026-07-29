9a69fe246e9761bc4c8824ee4c1b10315fe67c5c
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-07-29 05:02:00 -0700

    Fix 0-read_file.py with documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a read_file function"""
+
+
 def read_file(filename=""):
+    """Read a text file and print its content to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
