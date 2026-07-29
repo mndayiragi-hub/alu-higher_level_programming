a96086f2df780beefb742745a59869085063578e
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-07-29 05:06:12 -0700

    Fix PEP8 blank line in 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,3 @@ def write_file(filename="", text=""):
     """Write a string to a text file and return number of chars written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
