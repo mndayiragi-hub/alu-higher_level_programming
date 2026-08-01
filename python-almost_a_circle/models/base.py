e24d7b38491902ca4d76a5f221bcab328496aca3
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-08-01 10:53:10 -0700

    Add JSON conversion and instance loading methods

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,3 +38,36 @@ class Base:
 
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list represented by a JSON string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances from a JSON file."""
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                json_string = f.read()
+        except FileNotFoundError:
+            return []
+
+        list_dictionary = cls.from_json_string(json_string)
+
+        return [cls.create(**dictionary) for dictionary in list_dictionary]
