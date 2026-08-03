74d6a89b1fc4806e6ba493ecd5542e11f5e700e9
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-08-03 01:57:44 -0700

    Add model_state.py and 6-model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines the State class using SQLAlchemy"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class that represents a state, linked to MySQL table states"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
