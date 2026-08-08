8c316a5536e15983562f2aaa7134ec4ad94dc144
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-08-08 04:28:56 -0700

    Fix relationship between City and State

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines the City class using SQLAlchemy"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 
@@ -11,3 +12,4 @@ class City(Base):
                 autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", backref="cities")
